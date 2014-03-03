a015b7d9d5444ff418b92e123d7917386cc2a4ce
Francisco Villasanti <villasanti.francisco@gmail.com>
2014-03-03 16:35:47 -0300

    changes added to server and client app

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
- var db = openDatabase("test", "1.0", "BD Prueba", 1024);  // Open SQLite Database
+// var db = openDatabase("test", "1.0", "BD Prueba", 1024);  // Open SQLite Database
 
 $(document).ready(function(){
 
@@ -6,9 +6,9 @@ $(document).ready(function(){
 
 $("#formulario").submit(function(event){
 	event.preventDefault();
-	$("#usuario").hide("slow",function(){});
-	$("#pass").hide("slow",function(){});
-	$("#spin").show("slow",function(){});
+	$("#usuario").hide("fast",function(){});
+	$("#pass").hide("fast",function(){});
+	$("#spin").show("fast",function(){});
 
 	verificarUsuario($("#usuario").val(),$("#pass").val(), darRespuesta);
 	/* 
@@ -19,7 +19,39 @@ $("#usuario").show("slow",function(){});
 
 });
 
+var datosLogin = function  (nomus, passus) {
+	this.nusuario = nomus;
+	this.pusuario = passus;
+	this.toString = function () {
+		return " username: "+this.nusuario+" userpass: "+this.pusuario;	
+	};
+};
+
 var verificarUsuario = function(us,pass,callback){
+
+
+	var sesion = new datosLogin(us,pass);
+	alert("json "+JSON.stringify(sesion));
+
+	$.ajax ({
+		dataType: "jsonp",
+        jsonpCallback: "algo",
+        jsonp: false,
+		type: "GET",
+		url: "http://localhost:2011/login.html",
+		contentType: "application/json; charset=utf-8",
+		dataType: "jsonp",
+		data: JSON.stringify(sesion),
+		success: function (data, status, jqXHR) {
+			alert("la respuesta del server ");
+		},
+		error: function(xhrequest, ErrorText, thrownError) {
+            alert("eror:  " + thrownError + " : " + ErrorText);
+        }
+
+	});
+
+	/*
 	db.transaction(function(t){
 		t.executeSql("SELECT * FROM usuarios where id = ? and pass= ?;",
 			[us,pass],
@@ -28,23 +60,35 @@ var verificarUsuario = function(us,pass,callback){
 			}
 		);
 	});
+*/
+};
+
+var algo = function(data) {
+	alert(JSON.stringify(data));
 };
 
 var darRespuesta = function(result){
-	if(result.rows.length != 0 ){
+	if(result != 0 ){
 		$("#alcahuete").text("Exito! tu usuario y password son validos");
-		$("#alcahuete").removeClass("label-default");
-		$("#alcahuete").removeClass("label-danger");
-		$("#alcahuete").addClass("label-success");
+		$("#alcahuete").removeClass("alert-info");
+		$("#alcahuete").removeClass("alert-danger");
+		$("#alcahuete").addClass("alert-success");
 
 	}else{
-				$("#alcahuete").text("Error! tu usuario y password NO son validos");
-		$("#alcahuete").removeClass("label-default");
-		$("#alcahuete").removeClass("label-success");
-		$("#alcahuete").addClass("label-danger");
+		$("#alcahuete").text("Error! tu usuario y password NO son validos");
+		$("#alcahuete").removeClass("alert-info");
+		$("#alcahuete").removeClass("alert-success");
+		$("#alcahuete").addClass("alert-danger");
 		$("#usuario").show("slow",function(){});
 		$("#pass").show("slow",function(){});
 		$("#spin").fadeOut("slow");
 	};
 };
+
+var mostrarError = function(data){
+	$("#alcahuete").text("Error en http request");
+		$("#alcahuete").removeClass("alert-info");
+		$("#alcahuete").removeClass("alert-success");
+		$("#alcahuete").addClass("alert-danger");
+}
 
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var http = require("http");
+var server = http.createServer(app);
+var mysql = require("mysql");
+
+var cliente = mysql.createConnection({
+	user :"queryuser",
+	password : "undomiel",
+	host :"192.168.0.103",
+	port :"3306",
+	database :"lancaster_test"
+});
+
+
+var usuario = function (id, nom, pass) {
+	this.idus = id;
+	this.nusuario = nom;
+	this.passus = pass;
+	this.toString = function () {
+		return "id: "+this.idus+" nomusuario: "+this.nusuario+" pusuario: "+this.pusuario;	
+	};
+};
+
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+});
+
+
+var mostrarUS = function(req, res){
+	var arreglo = new Array();
+//	console.log("pedido "+JSON.stringify(res));
+	cliente.query("select * from usuarios;", function (err,results,fields) {
+		if(err){
+			res.send("Error en query! "+err.message);
+		}else{
+			console.log("buena!!!");
+			for(var i=0; i < results.length; i++){
+				var us = new usuario(results[i].idusuario,results[i].nusuario, results[i].pusuario);
+				arreglo.push(us);
+			};
+			console.log(arreglo.valueOf());
+			res.send(arreglo);
+		}
+	});
+};
+
+app.get("/login.html", mostrarUS);
+
+server.listen(2011, function () {
+	console.log("Node server running on http://localhost:2011");
+	cliente.connect(function(err) {
+		if(err){
+			console.log("error en conexion con mysql!! "+err.message);
+		}
+	});
+});
